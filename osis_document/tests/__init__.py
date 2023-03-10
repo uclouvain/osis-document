b17dd38fbb2bca98ee9ab27c71d5b9e8741dd08f
Julien Cougnaud <julien.cougnaud@makina-corpus.com>
2023-03-10 12:49:30 +0100

    Bulk tokens creation

--- osis_document/tests/__init__.py
+++ osis_document/tests/__init__.py
@@ -23,3 +23,31 @@
 #    see http://www.gnu.org/licenses/.
 #
 # ##############################################################################
+
+import json
+from contextlib import contextmanager
+
+from django.db import connections
+from django.test.utils import CaptureQueriesContext
+
+
+class QueriesAssertionsMixin:
+    @contextmanager
+    def assertNumQueriesLessThan(self, value, using="default", verbose=False):
+        with CaptureQueriesContext(connections[using]) as context:
+            yield  # your test will be run here
+        if verbose:
+            msg = "\r\n%s" % json.dumps(context.captured_queries, indent=4)
+        else:
+            msg = None
+        self.assertLess(len(context.captured_queries), value, msg=msg)
+
+    @contextmanager
+    def assertQueriesTimingLessThan(self, value, using="default", verbose=False):
+        with CaptureQueriesContext(connections[using]) as context:
+            yield  # your test will be run here
+        if verbose:
+            msg = "\r\n%s" % json.dumps(context.captured_queries, indent=4)
+        else:
+            msg = None
+        self.assertLess(sum(float(q["time"]) for q in context.captured_queries), value, msg=msg)
