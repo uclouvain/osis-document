3c2a2f8e94e805d8315ecec0df3863fa8a52dc30
albrugnetti <alessandro.brugnetti@uclouvain.be>
2023-03-22 12:02:26 +0000

    Deploying to gh-pages from  @ fc624739982c71a9fbfea0871498af624d807f61 🚀

--- osis_document/tests/document_test/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'osis_document.tests.document_test.apps.DocumentTestConfig'
--- osis_document/tests/document_test/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DocumentTestConfig(AppConfig):
-    name = 'osis_document.tests.document_test'
