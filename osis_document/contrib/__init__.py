9d2251b62ff28dbb4e16be435e1238c13270c0d0
Vincent Brichant <he201583@students.ephec.be>
2023-05-04 16:00:42 +0200

    Correction : imports supprimé par l'optimisation des import

--- osis_document/contrib/__init__.py
+++ osis_document/contrib/__init__.py
@@ -23,6 +23,16 @@
 #  see http://www.gnu.org/licenses/.
 #
 # ##############################################################################
+from osis_document.contrib.fields import FileField
+from osis_document.contrib.forms import FileUploadField
+from osis_document.contrib.post_processing.converter_registry import ConverterRegistry
+
+from osis_document.contrib.post_processing.converters.converter import Converter
+from osis_document.contrib.post_processing.converters.converter_image_to_pdf import ConverterImageToPdf
+from osis_document.contrib.post_processing.converters.converter_text_document_to_pdf import ConverterTextDocumentToPdf
+from osis_document.contrib.post_processing.merger import merger
+from osis_document.contrib.serializers import FileField as FileFieldSerializer
+from osis_document.contrib.widgets import FileUploadWidget
 
 __all__ = [
     'FileField',
