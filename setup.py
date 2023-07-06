e01294336d54616b9c4befca648694d10ae4e8bb
Julien Cougnaud <julien.cougnaud@makina-corpus.com>
2023-07-06 09:49:51 +0200

    [0.6] Fix the pdf editing

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='OSIS Document',
-    version='0.5',
+    version='0.6',
     description='Document management API and widget',
     url='http://github.com/uclouvain/osis-document',
     author='Université catholique de Louvain',
