57f25a2988252ac0e1196ad8057df8f2a92d62a0
albrugnetti <alessandro.brugnetti@uclouvain.be>
2025-09-04 14:21:35 +0200

    Add JSONRenderer for Default renderer_classes

--- backoffice/settings/base.py
+++ backoffice/settings/base.py
@@ -110,6 +110,9 @@ DATABASES = {
 }
 
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
     'DEFAULT_PERMISSION_CLASSES': (
         'backoffice.settings.rest_framework.permissions.APIKeyPermission',
     ),
